951b40c81136b8efcaefae0e5d049eef2013025f
Emmanuel Durin <emmanueldurin@gmail.com>
2022-11-23 14:37:49 +0100

    Implementation of Web Api

---
+++ src/Api/Controllers/IdentityController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+  [Route("identity")]
+  // ED les appels nécessitent l'authentification (la fourniture du token JWT)
+  [Authorize]
+  public class IdentityController : ControllerBase
+  {
+    [HttpGet]
+    public IActionResult Get()
+    {
+      return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+    }
+  }
+
+}
--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,8 +1,23 @@
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+// ED
+// 1) l'appel des APi sécurisées par [Authorize] utilise la vérification du token fournie par le client'appelant
+// 2) Le Token est vérifié auprès de l'autorité = Identity Provider
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+      options.Authority = "https://localhost:5001";
+
+      options.TokenValidationParameters = new TokenValidationParameters
+      {
+        ValidateAudience = false
+      };
+    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,7 +32,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+// ED dds the authentication middleware to the pipeline
+// so authentication will be performed automatically on every call into the host.
+app.UseAuthentication();
+// ED UseAuthorization adds the authorization middleware to make sure,
+// our API endpoint cannot be accessed by anonymous clients. 
 app.UseAuthorization();
 
 app.MapControllers();
