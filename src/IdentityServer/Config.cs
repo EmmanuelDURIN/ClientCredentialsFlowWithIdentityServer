8b952fc7be48ac2c45998212913995dc3872e8bb
Emmanuel Durin <emmanueldurin@gmail.com>
2022-11-23 14:21:10 +0100

    Creation and implementation of IdentityServer with definition of client and scope

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -4,17 +4,39 @@ namespace IdentityServer;
 
 public static class Config
 {
-    public static IEnumerable<IdentityResource> IdentityResources =>
-        new IdentityResource[]
-        { 
+  public static IEnumerable<IdentityResource> IdentityResources =>
+      new IdentityResource[]
+      {
             new IdentityResources.OpenId()
+      };
+
+  public static IEnumerable<ApiScope> ApiScopes =>
+        new List<ApiScope>
+        {
+            new ApiScope("api1", "My API")
         };
 
-    public static IEnumerable<ApiScope> ApiScopes =>
-        new ApiScope[]
-            { };
+  public static IEnumerable<Client> Clients =>
+      new List<Client>
+      {
+        new Client
+        {
+          // ED on définit la paire
+          // {client_id, client_secret }
+            ClientId = "client",
 
-    public static IEnumerable<Client> Clients =>
-        new Client[] 
-            { };
+            // no interactive user, use the clientid/secret for authentication
+            // ED fourniture d'un flow d'authentification
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+            // secret for authentication
+            ClientSecrets =
+            {
+                new Secret("secret".Sha256())
+            },
+
+            // scopes that client has access to
+            AllowedScopes = { "api1" }
+        }
+      };
 }
